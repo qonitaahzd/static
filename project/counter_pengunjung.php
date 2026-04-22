5b3dc4e01bf40adaeec30fc6d70d0ab29a4e2cf4
qonitahzd <qanitaahzd@gmail.com>
2026-04-22 20:55:30 +0700

    qonita

--- project/counter_pengunjung.php
+++ project/counter_pengunjung.php
@@ -1,5 +1,19 @@
 <?php
-class Produk {
 
-    public static $jumlahProduk = 0;
+class Pengunjung {
+
+    public static $jumlah = 0;
+
+    public function __construct() {
+        self::$jumlah++;
+    }
+
 }
+
+$p1 = new Pengunjung();
+$p2 = new Pengunjung();
+$p3 = new Pengunjung();
+
+echo "Jumlah Pengunjung: " . Pengunjung::$jumlah;
+
+?>
