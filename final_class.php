6eae27b0248146541fa6336637ab9b24de69a433
qonitahzd <qanitaahzd@gmail.com>
2026-04-22 11:38:30 +0700

    first commit

---
+++ final_class.php
@@ -0,0 +1,15 @@
+<?php
+
+final class Database {
+    
+public function connect() {
+        echo "Koneksi database";
+    }
+}
+
+// ERROR jika dilakukan
+class MyDB extends Database {
+    
+}
+
+?>
